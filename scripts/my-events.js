1bb1d50a0d2092c34d7bb264800519caba161822
Willy Liao <willyliao0425@gmail.com>
2022-03-18 23:56:11 -0700

    created util.js for utility functions

--- scripts/my-events.js
+++ scripts/my-events.js
@@ -25,37 +25,7 @@ function populateMyEventsList() {
 
 }
 
-// Formats Date Object into `month day`
-function formatDate(date) {
-  const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ];
 
-  // date formatted
-  const month = monthNames[date.getMonth()];
-  const day = date.getDate();
-  const formattedDate = `${month} ${day}`;
-
-  return formattedDate;
-}
-
-// Formats Date Object into `hour:minute am/pm`
-function formatTime(date) {
-  // convert hour from 24h to 12h format
-  let hour = date.getHours();
-
-  const ampm = hour < 12 ? 'am' : 'pm';
-
-  hour = hour % 12;
-  hour = hour ? hour : 12;
-
-  // format minutes to have 2 digits
-  let minutes = date.getMinutes();
-  minutes = minutes < 10 ? `0${minutes}` : minutes;
-  const time = `${hour}:${minutes} ${ampm}`;
-
-  return time;
-}
 
 // Creates an event list item for the list group
 function createEventListItem(templateClone, eventID, type, date, time, venue) {
@@ -151,5 +121,5 @@ function createEventCards(eventDoc, partyMembers, isHost) {
 
 // load event-list-item.html template 
 loadComponentToId("#eventCardTemplate", "./components/event-list-item.html");
-appendComponentToId("#confirmationModal", "./components/confirmation-modal.html");
+loadComponentToId("#confirmationModal", "./components/confirmation-modal.html");
 populateMyEventsList();
